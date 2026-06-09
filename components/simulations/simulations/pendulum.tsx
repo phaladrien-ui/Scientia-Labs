f89055d39f362ddfd536553b72af022ea3185852
phaladrien-ui <phaladrien@github.com>
2026-06-09 14:48:29 +0100

    Change Pendulum export from default to named

--- components/simulations/simulations/pendulum.tsx
+++ components/simulations/simulations/pendulum.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function PendulumSimulation() {
+export function Pendulum() {
   return (
     <div className="p-4 text-center">
       <h3 className="text-lg font-semibold mb-2">Simulation de Pendule</h3>
